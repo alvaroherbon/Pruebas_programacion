5b5a16ca52c319f5fd7184804de82a40cd7ed65f
alvaroherbon <57678071+alvaroherbon@users.noreply.github.com>
2021-03-04 00:38:27 +0100

    añade main, cabecera codificar

---
+++ Java/codificado_romano.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class codificado_romano {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+
+    }
+
+    public String codificar(String entrada, int valor) {
+
+    }
+}
