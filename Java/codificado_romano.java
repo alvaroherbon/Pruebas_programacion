eb3ce8780aea8ab6cf0d3b4b8216621245b1b6bd
alvaroherbon(linux) <alvaroherbon93@gmail.com>
2021-03-04 20:22:17 +0100

    añade funcion decodificar

--- Java/codificado_romano.java
+++ Java/codificado_romano.java
@@ -4,31 +4,58 @@ import java.util.Scanner;
 
 public class codificado_romano {
 
-  public static final String abecedario = "abcdefghijklmnñopqrstuvwxyz";
+    public static final String abecedario = "abcdefghijklmnñopqrstuvwxyz";
 
-  public static void main(String[] args) {
-    System.out.println("Introduce la frase a codificar");
-    Scanner entrada = new Scanner(System.in);
-    String oracion = entrada.nextLine();
-    System.out.println("Introduce el valor de codificado");
-    int valor = entrada.nextInt();
-    String[] palabras = oracion.split(" ");
-    for (int i = 0; i < palabras.length; i++) {
-      codificar(palabras[i], valor);
+    public static void main(String[] args) {
+        System.out.println("Introduce la frase a codificar");
+        Scanner entrada = new Scanner(System.in);
+        String oracion = entrada.nextLine();
+        System.out.println("Introduce el valor de codificado");
+        int valor = entrada.nextInt();
+        String[] palabras = oracion.split(" ");
+        for (int i = 0; i < palabras.length; i++) {
+            System.out.print(codificar(palabras[i], valor));
+        }
+        System.out.println("");
+        System.out.println("introduce la oracion a descodificar");
+        Scanner entrada2 = new Scanner(System.in);
+        String respuesta = entrada2.nextLine();
+        System.out.println("introduce el valor");
+        int valor2 = entrada2.nextInt();
+        String[] palabras_deco = respuesta.split(" ");
+
+        for (int i = 0; i < palabras.length; i++) {
+            System.out.print(decodificar(palabras_deco[i], valor2));
+        }
+
+        entrada.close();
+        entrada2.close();
     }
-    entrada.close();
-  }
 
-  public static void codificar(String entrada, int valor) {
-    String respuesta = "";
-    for (int i = 0; i < entrada.length(); i++) {
-      int pos_letra = abecedario.lastIndexOf(entrada.charAt(i));
-      char letra = abecedario.charAt((pos_letra + valor) % 27);
-      respuesta = respuesta + letra;
-      // char letra = (char) (entrada.charAt(i) + valor);
-      // respuesta = respuesta + letra;
+    public static String codificar(String entrada, int valor) {
+        String respuesta = "";
+        for (int i = 0; i < entrada.length(); i++) {
+            int pos_letra = abecedario.lastIndexOf(entrada.charAt(i));
+            char letra = abecedario.charAt((pos_letra + valor) % 27);
+            respuesta = respuesta + letra;
+            // char letra = (char) (entrada.charAt(i) + valor);
+            // respuesta = respuesta + letra;
 
+        }
+        return (respuesta + " ");
+        // System.out.print(respuesta + " ");
     }
-    System.out.print(respuesta + " ");
-  }
+
+    public static String decodificar(String entrada, int valor) {
+        String respuesta = "";
+        for (int i = 0; i < entrada.length(); i++) {
+            int pos_letra = abecedario.lastIndexOf(entrada.charAt(i));
+            char letra = abecedario.charAt((pos_letra - valor) % 27);
+            respuesta = respuesta + letra;
+            // char letra = (char) (entrada.charAt(i) + valor);
+            // respuesta = respuesta + letra;
+
+        }
+        return (respuesta + " ");
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Pruebas_programacion
+
 Diferentes proyectos en diferentes lenguajes
+
+JAVA :
+
+- codificado_romano: pequeño programa escrito en Java para realizar la codificación o descodificación utilizando el sistema romano de criptografía.
