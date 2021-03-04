74c646b0bb54f510dfbc9203a2217f4361435e9f
alvaroherbon <57678071+alvaroherbon@users.noreply.github.com>
2021-03-04 13:07:37 +0100

    FINISHED

--- Java/codificado_romano.java
+++ Java/codificado_romano.java
@@ -1,14 +1,28 @@
 package Java;
 
+import java.util.Scanner;
+
 public class codificado_romano {
 
-    public static void main(String[] args) {
-        System.out.println("hola mundo");
+  public static final String abecedario = "abcdefghijklmnñopqrstuvwxyz";
+
+  public static void main(String[] args) {
+    System.out.println("Introduce la palabra a codificar");
+    Scanner entrada = new Scanner(System.in);
+    String palabra = entrada.nextLine();
+    System.out.println("Introduce el valor de codificado");
+    int valor = entrada.nextInt();
+    codificar(palabra, valor);
+
+  }
+
+  public static void codificar(String entrada, int valor) {
+    String respuesta = "";
+    for (int i = 0; i < entrada.length(); i++) {
+      char letra = (char) (entrada.charAt(i) + valor);
+      respuesta = respuesta + letra;
 
     }
-
-    public String codificar(String entrada, int valor) {
-        return "hola";
-
-    }
+    System.out.println(respuesta);
+  }
 }
