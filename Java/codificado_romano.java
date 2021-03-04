f662fc16e583fa57aa93b32a9ab47b89daefe25b
alvaroherbon <57678071+alvaroherbon@users.noreply.github.com>
2021-03-04 13:26:28 +0100

    añade funcionalidad oraciones

--- Java/codificado_romano.java
+++ Java/codificado_romano.java
@@ -7,22 +7,28 @@ public class codificado_romano {
   public static final String abecedario = "abcdefghijklmnñopqrstuvwxyz";
 
   public static void main(String[] args) {
-    System.out.println("Introduce la palabra a codificar");
+    System.out.println("Introduce la frase a codificar");
     Scanner entrada = new Scanner(System.in);
-    String palabra = entrada.nextLine();
+    String oracion = entrada.nextLine();
     System.out.println("Introduce el valor de codificado");
     int valor = entrada.nextInt();
-    codificar(palabra, valor);
-
+    String[] palabras = oracion.split(" ");
+    for (int i = 0; i < palabras.length; i++) {
+      codificar(palabras[i], valor);
+    }
+    entrada.close();
   }
 
   public static void codificar(String entrada, int valor) {
     String respuesta = "";
     for (int i = 0; i < entrada.length(); i++) {
-      char letra = (char) (entrada.charAt(i) + valor);
+      int pos_letra = abecedario.lastIndexOf(entrada.charAt(i));
+      char letra = abecedario.charAt((pos_letra + valor) % 27);
       respuesta = respuesta + letra;
+      // char letra = (char) (entrada.charAt(i) + valor);
+      // respuesta = respuesta + letra;
 
     }
-    System.out.println(respuesta);
+    System.out.print(respuesta + " ");
   }
 }
